a5904bcebc44486f1d38be948dad12c151541ee7
naimeeshgit <iamthennt@gmail.com>
2021-04-26 13:11:32 +0530

    Initialized transaction.h

---
+++ transaction.h
@@ -0,0 +1,20 @@
+#ifndef _Transaction_H
+#define _Transaction_H
+
+typedef struct Transaction Trans_node; 
+
+struct Transaction{
+
+    int S_UID ;
+    int R_UID ;
+    double Amount ;
+    Trans_node * pNext;
+
+};
+
+
+
+
+
+
+#endif
