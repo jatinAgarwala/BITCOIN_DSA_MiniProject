5de7afdbfd56c34b4ee0e970fdc1378a64739b97
naimeeshgit <iamthennt@gmail.com>
2021-05-08 17:23:23 +0530

    added more functions to UI.c

--- UI.c
+++ UI.c
@@ -46,3 +46,69 @@ int UI_ValidateBlockChain(BlockChain B) {
     }
     else printf("Block chain has been validated\n");
 }
+
+void UI_PrintCurrentBlockInfo(BlockChain B) {
+    printf("Current block:-\n");
+    printf("\tBlock number: %d\n", B->CurrBlock->BlockNumber);
+    printf("\tpreviousBlockHash: %d\n", B->CurrBlock->PreviousBlockHash);
+    printf("\tNonce: %d\n", B->CurrBlock->Nonce);
+    printf("\tNumber of transactions: %d\n", B->CurrBlock->numTransaction);
+    printf("\tTransactions -\n");
+    for(int i=0; i<B->CurrBlock->numTransaction; i++) {
+        printf("\t\tSender UID: %d\n", B->CurrBlock->TransactionArray[i]->S_UID);
+        printf("\t\tReceiver UID: %d\n", B->CurrBlock->TransactionArray[i]->R_UID);
+        printf("\t\tAmount: %lf\n", B->CurrBlock->TransactionArray[i]->Amount);
+    }
+}
+
+void UI_PrintInfoBlock(BlockChain B) {
+    int blockNumber;
+    printf("Enter block number: ");
+    scanf("%d", &blockNumber);
+    Block ptr = B->Head;
+    while(ptr!=NULL) {
+        if(ptr->BlockNumber==BlockNumber) {
+            printf("\tBlock number: %d\n", ptr->BlockNumber);
+            printf("\tpreviousBlockHash: %d\n", ptr->PreviousBlockHash);
+            printf("\tNonce: %d\n", ptr->Nonce);
+            printf("\tNumber of transactions: %d\n", ptr->numTransaction);
+            printf("\tTransactions -\n");
+            for(int i=0; i<ptr->numTransaction; i++) {
+                printf("\t\tSender UID: %d\n", ptr->TransactionArray[i]->S_UID);
+                printf("\t\tReceiver UID: %d\n", ptr->TransactionArray[i]->R_UID);
+                printf("\t\tAmount: %lf\n", ptr->TransactionArray[i]->Amount);
+            }
+            return;
+        }
+        ptr = ptr->Next;
+    }
+}
+
+void UI_PrintUserInfo(UsersArray UA , UserHashTable UHT) {
+    int UID;
+    printf("Enter UID: ");
+    scanf("%d", &UID);
+    User userptr = FindUser(UID, UA, UHT);
+    printf("User details -\n");
+    printf("\tUID: %d\n", UID);
+    printf("\tBalance: %lf\n", userptr->Balance);
+    printf("\tJoin date/time: %s\n", userptr->JoinDateTime);
+    printf("\tTransaction History -\n");
+    U_transactions ptr = userptr->TransactionHistory;
+    while(ptr!=NULL) {
+        printf("\t\tUID: %d\n", ptr->UID);
+        printf("\t\tAmount: %lf\n", ptr->Amount);
+        ptr = ptr->Next;
+    }
+}
+
+void UI_PrintAll_UIDs(UsersArray UA) {
+    printf("UIDs -\n");
+    for(int i=0; i<UA->CurrIndex; i++) {
+        printf("\t%d\n", UA->ArrayOfUsers[i]->UID);
+    }
+}
+
+void UI_PrintNumBlocks(BlockChain B) {
+    printf("Number of blocks in blockchain: %d", B->NumBlocks);
+}
