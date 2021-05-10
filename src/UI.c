9558978f82592e50f8159b0492bf94a6e064ab8e
naimeeshgit <iamthennt@gmail.com>
2021-05-10 17:26:01 +0530

    fixed bugs

--- src/UI.c
+++ src/UI.c
@@ -27,7 +27,7 @@ void UI_Transact(BlockChain B, UsersArray UA, UserHashTable UHT)
         return;
     }
 
-    printf("Enter receiver's UID: ");
+    printf("Enter receiver's UID: ");        
     scanf("%d", &R_UID);
     User R = FindUser(R_UID,UA,UHT);
     if(R == NULL || R->UID != R_UID)
@@ -91,7 +91,7 @@ void UI_PrintCurrentBlockInfo(BlockChain B)
     {
         printf("\t\tSender UID: %d\t", B->CurrBlock->TransactionArray[i]->S_UID);
         printf("\t\tReceiver UID: %d\t", B->CurrBlock->TransactionArray[i]->R_UID);
-        printf("\t\tAmount: %lg\n", B->CurrBlock->TransactionArray[i]->Amount);
+        printf("\t\tAmount: %lf\n", B->CurrBlock->TransactionArray[i]->Amount);
     }
     return;
 }
@@ -119,7 +119,7 @@ void UI_PrintInfoBlock(BlockChain B)
             {
                 printf("\t\tSender UID: %d\t", ptr->TransactionArray[i]->S_UID);
                 printf("\t\tReceiver UID: %d\t", ptr->TransactionArray[i]->R_UID);
-                printf("\t\tAmount: %lg\n", ptr->TransactionArray[i]->Amount);
+                printf("\t\tAmount: %lf\n", ptr->TransactionArray[i]->Amount);
             }
             return;
         }
@@ -144,7 +144,7 @@ void UI_PrintUserInfo(UsersArray UA , UserHashTable UHT)
 
     printf("User details -\n");
     printf("\tUID: %d\n", UID);
-    printf("\tBalance: %lg\n", userptr->Balance);
+    printf("\tBalance: %lf\n", userptr->Balance);
     printf("\tJoin date/time: %s\n", userptr->JoinDateTime);
     
     U_transactions ptr = userptr->TransactionHistory->Next;
@@ -158,11 +158,11 @@ void UI_PrintUserInfo(UsersArray UA , UserHashTable UHT)
     while(ptr!=NULL) 
     {
         if(ptr->Amount > 0)
-            printf("\t\tReceived %lg BTC from %d\n",ptr->Amount,ptr->UID);
+            printf("\t\tReceived %lf BTC from %d\n",ptr->Amount,ptr->UID);
         else if(ptr->Amount < 0)
-            printf("\t\tSent %lg BTC to %d\n",(ptr->Amount)*(-1),ptr->UID);
+            printf("\t\tSent %lf BTC to %d\n",(ptr->Amount)*(-1),ptr->UID);
         // printf("\t\tUID: %d\n", ptr->UID);
-        // printf("\t\tAmount: %lg\n", ptr->Amount);
+        // printf("\t\tAmount: %lf\n", ptr->Amount);
         ptr = ptr->Next;
     }
     return;
@@ -173,7 +173,7 @@ void UI_PrintAll_UIDs(UsersArray UA)
     printf("UIDs -\n");
     for(int i=0; i<UA->CurrIndex; i++) 
     {
-        printf("\t%d has Balance: %lg\n", UA->ArrayOfUsers[i]->UID, UA->ArrayOfUsers[i]->Balance);
+        printf("\t%d has Balance: %lf\n", UA->ArrayOfUsers[i]->UID, UA->ArrayOfUsers[i]->Balance);
     }
     return;
 }
